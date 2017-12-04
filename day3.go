ee361239eaa5631d6e585f80787c8f5aa9d441a8
Nate Denlinger <pitchinnate@gmail.com>
2017-12-04 11:04:22 -0500

    finish day 3 part 2

--- day3.go
+++ day3.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"math"
+	"fmt"
 	"log"
+	"math"
 )
 
 func day3_part1() {
@@ -42,32 +43,101 @@ func day3_part1() {
 func day3_part2() {
 	// input := 325489
 	//input := 747
-	maxSideLength := 5
-	totalNumbers := maxSideLength * maxSideLength
+	numberOfCircles := 5
+	maxSideLength := (numberOfCircles * 2) - 1
 	numbers := makeGrid(maxSideLength)
-	xPos := (maxSideLength / 2) + 1
-	yPos := xPos
-	log.Print(xPos)
-	log.Print(yPos)
-	log.Print(numbers)
-	log.Print(totalNumbers)
-	numbers[(yPos - 1)][(xPos - 1)] = 1
-	log.Print(numbers)
+	xPos := maxSideLength / 2
+	xCenterPos := xPos
+	numbers[xPos][xPos] = 1
 
-	changeX := 1
-	changeY := 0
+	numbers[xPos][(xPos + 1)] = 1
+	numbers[(xPos - 1)][(xPos + 1)] = 2
+	numbers[(xPos - 1)][xPos] = 4
+	numbers[(xPos - 1)][(xPos - 1)] = 5
+	numbers[xPos][(xPos - 1)] = 10
+	numbers[(xPos + 1)][(xPos - 1)] = 11
+	numbers[(xPos + 1)][xPos] = 23
+	numbers[(xPos + 1)][(xPos + 1)] = 25
 
-	for i:=1; i<=totalNumbers; i++ {
-		xPos += changeX
-		yPos += changeY
+	nextY := xPos + 1
+	nextX := xPos + 2
+
+	for circle := 3; circle <= numberOfCircles; circle++ {
+		currentMin := xCenterPos - circle + 1
+		currentMax := xCenterPos + circle - 1
+		direction := "new"
+
+		for direction != "done" {
+			newVal := getValues(numbers, nextX, nextY)
+			numbers[nextY][nextX] = newVal
+			if direction == "new" {
+				direction = "up"
+				nextY--
+			} else if direction == "up" {
+				if (nextY - 1) >= currentMin {
+					nextY--
+				} else {
+					direction = "left"
+					nextX--
+				}
+			} else if direction == "left" {
+				if (nextX - 1) >= currentMin {
+					nextX--
+				} else {
+					direction = "down"
+					nextY++
+				}
+			} else if direction == "down" {
+				if (nextY + 1) <= currentMax {
+					nextY++
+				} else {
+					direction = "right"
+					nextX++
+				}
+			} else if direction == "right" {
+				if (nextX + 1) <= currentMax {
+					nextX++
+				} else {
+					direction = "done"
+					nextX++
+				}
+			}
+		}
 	}
+	printGrid(numbers)
+}
 
+func getValues(numbers [][]int, xCord int, yCord int) int {
+	max := len(numbers) - 1
+	total := 0
+	for y := -1; y <= 1; y++ {
+		for x := -1; x <= 1; x++ {
+			newYCord := yCord + y
+			newXCord := xCord + x
+			if newYCord >= 0 && newXCord >= 0 && newYCord <= max && newXCord <= max {
+				total += numbers[newYCord][newXCord]
+			}
+		}
+	}
+	return total
 }
 
 func makeGrid(sideLength int) [][]int {
 	numbers := make([][]int, sideLength)
-	for i:=0; i<sideLength; i++ {
-		numbers[i] = make([]int, sideLength)
+	for y := 0; y < sideLength; y++ {
+		numbers[y] = make([]int, sideLength)
+		for x := 0; x < sideLength; x++ {
+			numbers[y][x] = 0
+		}
 	}
 	return numbers
+}
+
+func printGrid(numbers [][]int) {
+	for y := 0; y < len(numbers); y++ {
+		for x := 0; x < len(numbers); x++ {
+			fmt.Printf("%7d", numbers[y][x])
+		}
+		fmt.Print("\n")
+	}
 }
