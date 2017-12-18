02bda56b805e28790acda725806ee4e17e78f814
Nate Denlinger <pitchinnate@gmail.com>
2017-12-18 10:22:32 -0500

    day14 done

--- day14.go
+++ day14.go
@@ -55,19 +55,67 @@ func day14_part2() {
 		lines = append(lines, tempString)
 	}
 	var grid [128][128]int
-	for y := 0; y < (len(lines) - 1); y++ {
+	for y := 0; y < 128; y++ {
 		letters := strings.Split(lines[y], "")
-		for x := 0; x < (len(letters) - 1); x++ {
+		for x := 0; x < 128; x++ {
 			tempInt, _ := strconv.Atoi(letters[x])
 			grid[y][x] = tempInt
 		}
 	}
 	//http://gregtrowbridge.com/a-basic-pathfinding-algorithm/
+	group_count := 0
+	//displayGrid(grid)
 	for y := 0; y < 128; y++ {
 		for x := 0; x < 128; x++ {
-
+			if grid[y][x] == 1 {
+				group_count++
+				grid = findPath(x, y, grid)
+			}
 		}
 	}
+	log.Print(group_count)
+}
+
+func findPath(xCord int, yCord int, grid [128][128]int) [128][128]int {
+	var queue []Point
+	queue = append(queue, Point{xCord: xCord, yCord: yCord})
+	search := true
+	for search {
+		nextCord := queue[0]
+		queue = append(queue[:0], queue[1:]...)
+		grid[nextCord.yCord][nextCord.xCord] = 0
+
+		if nextCord.yCord > 0 && grid[(nextCord.yCord - 1)][nextCord.xCord] == 1 {
+			queue = append(queue, Point{xCord: nextCord.xCord, yCord: (nextCord.yCord - 1)})
+		}
+		if nextCord.yCord < 127 && grid[(nextCord.yCord + 1)][nextCord.xCord] == 1 {
+			queue = append(queue, Point{xCord: nextCord.xCord, yCord: (nextCord.yCord + 1)})
+		}
+		if nextCord.xCord > 0 && grid[nextCord.yCord][(nextCord.xCord-1)] == 1 {
+			queue = append(queue, Point{xCord: (nextCord.xCord - 1), yCord: nextCord.yCord})
+		}
+		if nextCord.xCord < 127 && grid[nextCord.yCord][(nextCord.xCord+1)] == 1 {
+			queue = append(queue, Point{xCord: (nextCord.xCord + 1), yCord: nextCord.yCord})
+		}
+		if len(queue) == 0 {
+			search = false
+		}
+	}
+	return grid
+}
+
+func displayGrid(grid [128][128]int) {
+	for y := 0; y < 128; y++ {
+		for x := 0; x < 128; x++ {
+			fmt.Printf("%d", grid[y][x])
+		}
+		fmt.Print("\n")
+	}
+}
+
+type Point struct {
+	xCord int
+	yCord int
 }
 
 func day14_input2(input string) ([256]int, []int) {
